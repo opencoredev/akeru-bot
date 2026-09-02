694fb48830a4d7d67b3e328afa6a3037e8cee769
Leo Orel <leodoesdev@gmail.com>
2026-09-01 20:18:50 -0400

    test(channels): cover composed runtime delivery store

--- apps/server/src/orchestration/runtimeLayer.test.ts
+++ apps/server/src/orchestration/runtimeLayer.test.ts
@@ -2,22 +2,21 @@ import { assert, it } from "@effect/vitest";
 import * as NodeServices from "@effect/platform-node/NodeServices";
 import * as Effect from "effect/Effect";
 import * as Layer from "effect/Layer";
-import * as Option from "effect/Option";
 
 import * as ChannelDeliveryStore from "../channels/ChannelDeliveryStore.ts";
 import * as ServerConfig from "../config.ts";
 import { SqlitePersistenceMemory } from "../persistence/Layers/Sqlite.ts";
 import * as RepositoryIdentityResolver from "../project/RepositoryIdentityResolver.ts";
-import { OrchestrationInfrastructureLayerLive } from "./runtimeLayer.ts";
+import { OrchestrationLayerLive } from "./runtimeLayer.ts";
 
 it.effect("provides channel delivery storage to the server runtime", () =>
   Effect.gen(function* () {
-    const store = yield* Effect.serviceOption(ChannelDeliveryStore.ChannelDeliveryStore);
+    const store = yield* ChannelDeliveryStore.ChannelDeliveryStore;
 
-    assert.isTrue(Option.isSome(store));
+    assert.isFunction(store.claim);
   }).pipe(
     Effect.provide(
-      OrchestrationInfrastructureLayerLive.pipe(
+      OrchestrationLayerLive.pipe(
         Layer.provideMerge(RepositoryIdentityResolver.layer),
         Layer.provideMerge(SqlitePersistenceMemory),
         Layer.provideMerge(
